07b2dfde9135ff7ed8ceb093d7996f44e686a5ec
hemanth7301 <saihemanthsakhamuri@gmail.com>
2024-04-07 12:12:14 +0530

    Thread Creation

---
+++ Thread Creation Part 1/ApproachOne.java
@@ -0,0 +1,17 @@
+public class ApproachOne {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("we are now in thread " + Thread.currentThread().getName());
+                System.out.println("we are now in thread " + Thread.currentThread().getPriority());
+            }
+        });
+        thread.setName("WorkerThread");
+
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+    }
+}
